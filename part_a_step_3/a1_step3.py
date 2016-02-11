08b9d644ace8d0371b91fa468f8fa2f2c74048cf
Yzonix <yzonix@gmail.com>
2016-02-11 10:33:24 +0100

    astep3: reimplement start stop symbol function

--- part_a_step_3/a1_step3.py
+++ part_a_step_3/a1_step3.py
@@ -1,4 +1,6 @@
 import argparse
+import re
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('training_corpus', type=str, help='text file of training corpus')
@@ -8,15 +10,27 @@ parser.add_argument('-smoothing', type=str, help='smoothing algorithm', default=
 args = parser.parse_args()
 
 
-def get_vocabulary_size(training_corpus):
+def get_vocabulary_size(text_filename):
     """Returns the amount of words in a text file"""
     word_count = 0
 
-    f = open(training_corpus)
+    f = open(text_filename)
     for line in f:
         words = line.split()
         word_count += len(words)
     return word_count
+
+
+def insert_start_stop(text_filename):
+    """Opens a file and inserts a START and STOP symbol on every double+
+    new line"""
+    text = open(text_filename, 'r').read()
+    text = re.sub(r'(\n\n)+', ' 0STOP0 0START0 ', text)
+    text = re.sub(r'(\n)', ' ', text)
+    text = '<START>' + text
+    text = text[:-9]
+    return text
+
 
 if __name__ == "__main__":
     exit(1)
